c530f630f8ded42ca74e0c6b25b41b1dd9aa1508
jagadeesh-08 <yarrajagadeesh.min18@itbhu.ac.in>
2021-09-03 22:07:21 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Map from "./components/map.js"
+import React, {useState ,useEffect} from 'react'
+import Loader from "./components/Loader.js";
+function App() {
+
+  const [eventData,setEventData] = useState([]);
+
+  const [loading,setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+      setEventData(events)
+      setLoading(false)
+    }
+    fetchEvents()
+    //console.log(eventData);
+  }, [])
+
+  return (
+    <div>
+     
+      {!loading? <Map eventData={eventData}/> : <Loader />}
+    </div>
+  );
+}
+
+export default App;
